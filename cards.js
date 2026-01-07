f64694bcc348c78f31ccc6ea5b9280d4cf003717
Rahul Jadhav <rahuljadhav.dev@gmail.com>
2026-01-07 18:46:18 +0530

    some topics covered

--- cards.js
+++ cards.js
@@ -12,29 +12,46 @@ form.addEventListener("submit", function (event) {
     let profile = document.createElement("div");
     profile.classList.add("profile");
 
-    let img=document.createElement("img");
-    img.setAttribute("src",inputs[0].value);
+    let img = document.createElement("img");
+    img.setAttribute("src", inputs[0].value);
 
-    let h3=document.createElement("h3");
-    let h5=document.createElement("h5");
-    let p=document.createElement("p");
+    let h3 = document.createElement("h3");
+    let h5 = document.createElement("h5");
+    let p = document.createElement("p");
 
-    h3.textContent=inputs[1].value;
-    h5.textContent=inputs[2].value;
-    p.textContent=inputs[3].value;
+    h3.textContent = inputs[1].value;
+    h5.textContent = inputs[2].value;
+    p.textContent = inputs[3].value;
 
-    profile.appendChild(img);
-    card.appendChild(profile);
-    card.appendChild(h3);
-    card.appendChild(h5);
-    card.appendChild(p);
+    // if ((zero.value !== null || zero.value !== "") && (one !== "" || one !== null) && (two !== null || two !== "") && (three !== null || three !== "")) {
+    //     profile.appendChild(img);
+    //     card.appendChild(profile);
+    //     card.appendChild(h3);
+    //     card.appendChild(h5);
+    //     card.appendChild(p);
 
-    main.appendChild(card);
+    //     main.appendChild(card);
+    // }
 
-    inputs.forEach(function(input){
-        if(input.type!=="submit"){
-            input.value="";
+    inputs.forEach(function (val) {
+        console.dir(val);
+        
+        if (val.value !== "") {
+            profile.appendChild(img);
+            card.appendChild(profile);
+            card.appendChild(h3);
+            card.appendChild(h5);
+            card.appendChild(p);
+
+            main.appendChild(card);
         }
     })
-   
+
+    inputs.forEach(function (input) {
+        if (input.type !== "submit") {
+            input.value = "";
+        }
+    })
+
 })
+
